208ab7259bad7c7f7e42fa1bd110df5cf8533232
Youssef-Mak <ymakb019@uottawa.ca>
2020-04-06 23:19:19 -0400

    Defined AST structure for let statements

---
+++ pkg/ast/ast.go
@@ -0,0 +1,51 @@
+package ast
+
+import (
+	"github.com/Youssef-Mak/baby-interpreter/pkg/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program will be the root node of every AST
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// LET STATEMENT -> "let <identifier> = <expression>;"
+
+type LetStatement struct {
+	Token token.Token // token.LET
+	Name  *Identifier
+	Value Expression
+}
+
+func (lStatement *LetStatement) statementNode()       {}
+func (lStatement *LetStatement) TokenLiteral() string { return lStatement.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // token.IDENT
+	Value string
+}
+
+func (ident *Identifier) expressionNode()      {}
+func (ident *Identifier) TokenLiteral() string { return ident.Token.Literal }
